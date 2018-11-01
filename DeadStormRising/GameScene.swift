b9951ae5b4b7f24c704635da4ef5c899157f94dd
Yury Shkoda <shkoda.yuri@gmail.com>
2018-11-01 10:07:57 -0400

    Initial commit

---
+++ DeadStormRising/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  DeadStormRising
+//
+//  Created by Yury on 11/1/18.
+//  Copyright © 2018 Yury Shkoda. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
